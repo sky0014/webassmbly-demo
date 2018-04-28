0e3929c27ed60750d8fc3eed369524948915ab51
sky0014 <sky-wang@qq.com>
2018-04-28 18:22:04 +0800

    add md5 demo

--- compile.sh
+++ compile.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run --rm -v $(pwd):/src trzeci/emscripten emcc -O3 -s WASM=1 -s EXTRA_EXPORTED_RUNTIME_METHODS='["cwrap"]' fib.c -o ./fib/fib.js
+docker run --rm -v $(pwd):/src trzeci/emscripten emcc -O3 -s WASM=1 -s EXTRA_EXPORTED_RUNTIME_METHODS='["cwrap"]' $1.c -o ./$1/$1.js
